e66055229d746a2f7ba4fd2230312c1e0977948b
Emptylight <1378990254@qq.com>
2026-05-14 01:18:02 +0800

    ci: 将获取版本号的方法换成PowerShell内置方法

--- .mise/tasks/changelog.ps1
+++ .mise/tasks/changelog.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 #MISE description="Generate changelog"
 
-$version = (mise x jq -- jq -r .version plugin.json)
+$version = (Get-Content plugin.json | ConvertFrom-Json).version
 git cliff -t $version
 prettier -w changelog.md
 git add changelog.md
